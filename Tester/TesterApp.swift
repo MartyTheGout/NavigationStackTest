06753d317e6eaef474e86870398c30488f380b5f
MartyTheGout <chihyung0@naver.com>
2024-03-28 20:25:47 +0900

    Initial Commit

---
+++ Tester/TesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesterApp.swift
+//  Tester
+//
+//  Created by marty.academy on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct TesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
